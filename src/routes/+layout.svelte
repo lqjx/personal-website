faf718c67c2174be57f0f0c33919b747cbddb574
KOZY <kozy@kozy.kozy>
2024-09-12 21:28:41 +0200

    Cleaned up unused imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script>
     import {fade} from "svelte/transition";
     import "../app.css";
-    import Navbar from "../lib/Navigation/Navbar.svelte";
-    import Footer from "../lib/Footer.svelte";
 </script>
 
 <div transition:fade class="bg-black text-neutral-50 font-jetbrains">
